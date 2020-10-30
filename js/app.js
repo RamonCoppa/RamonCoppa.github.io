504436f269ed1ff8fb6ea2129c7ca8312d9a56b5
jbyrne6 <jbyrne5094@gmail.com>
2020-10-30 01:16:40 -0700

    made images expand on click and tring responsive ramon photo

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ $(document).foundation()
 $('.sim-thumb').on('click', function() {
   $('#main-product-image').attr('src', $(this).data('image'));
 })
+
+const openModal = (element) => {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
